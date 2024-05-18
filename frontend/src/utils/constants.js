90cf457192c183e5ff735c96c9abce173c395ea8
kb87-98 <155606057+kb87-98@users.noreply.github.com>
2024-05-18 12:59:17 +0530

    Update constants.js

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://192.168.18.54:9093/";
+export const BASE_URL = "http://localhost:9093/";
 // export const getToken = () => {
 //   // console.log("getToken",localStorage.getItem("JWT"));
 //   return localStorage.getItem("JWT");
